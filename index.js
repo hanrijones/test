7b4551908c99f19d7bcabce752e7288c1cc1ab26
hanrijones <119027368+hanrijones@users.noreply.github.com>
2022-11-25 15:14:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,70 +1,11 @@
-const app = require("express")();
-const fsr = require('fs-reverse');
-const probe = require('probe-stream')()
-const cc = require("cryptocompare");
-const { HOST, PORT, APIKEY, USD } = require("./config/index");
+const HOST = "127.0.0.1";
+const PORT = process.env.PORT || 3000;
+const APIKEY = "fcb9c109c8da1be7f3f617487707c5cf97c421db90d778294f7baf6ccb7090a8";
+const USD = "USD";
 
-global.fetch = require("node-fetch");
-
-cc.setApiKey(APIKEY);
-
-let portfolio = 0;
-let tokens = [];
-let types = [];
-let amounts = [];
-
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      fsr('./db/transactions.csv')
-        .pipe(probe.createProbe())
-        .on('data', function (row) {
-          let newRow = row.split(',');
-          if (newRow[0] != 'timestamp' && newRow[0]) {
-            amount = (Number)(newRow[3]);
-            token = newRow[2];
-            type = newRow[1];
-            tokens.push(token);
-            amounts.push(amount);
-            types.push(type);
-          }
-        })
-        .on('end', async function () {
-          probe.end.bind(probe)
-          console.log("calculating...");
-          for (var i = 0; i < tokens.length; i = i + 1) {
-            await cc.price(tokens[i], USD).then((tokenPrice) => {
-              price = tokenPrice[USD] * amounts[i];
-              if (types[i] == "DEPOSIT") {
-                portfolio = portfolio + parseFloat(price)
-              }
-              if (types[i] == "WITHDRAWAL") {
-                portfolio = portfolio >= price ? portfolio - parseFloat(price) : portfolio;
-              }
-            });
-          }
-          console.log("price is ", portfolio);
-          console.log("Finished===================");
-        })
-        .on("error", function (error) {
-          console.log(error.message);
-        });
-
-    }, 20);
-  });
-}
-function resolveAfter4Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('4 seconds');
-    }, 4000);
-  });
-}
-
-async function asyncCall() {
-  const result1 = await resolveAfter2Seconds();
-}
-
-(async function () {
-  await asyncCall();
-})();
+module.exports = {
+  HOST,
+  PORT,
+  APIKEY,
+  USD
+};
